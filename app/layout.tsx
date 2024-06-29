e11561af4a90073580e01fbaf65247613666547b
andrewleee10 <78115069+andrewleee10@users.noreply.github.com>
2024-06-29 00:32:10 -0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Admin Dashboardd",
+  description: "Admin Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}><Navbar/>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+   <>
+      <h1 className="text-2xl">Dashboard</h1>
+      <Button className='text-blue-400' variant='default' size='lg'>Click me</Button>
+   </>
+  );
+}
